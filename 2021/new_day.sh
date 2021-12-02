c52fd66a5c3125e619dcbb4db86e2b28dd9dd16d
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-12-02 09:02:48 +0000

    add setup script

---
+++ 2021/new_day.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ $# -ne 1 ]]; then
+  echo "usage is $0 <day number>"
+  exit 1
+fi
+
+day=$1
+
+mkdir "$day"
+echo "#!/usr/bin/env awk -f" > "$day/solution.awk"
+chmod +x "$day/solution.awk"
+touch "$day/input.txt"
+touch "$day/example.txt"
