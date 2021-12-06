e323765fdf650e7e713b8a6df7f572b0ed68d44a
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-12-06 21:31:00 +0000

    auto pull input in setup script

--- 2021/new_day.sh
+++ 2021/new_day.sh
@@ -7,10 +7,15 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
+if [[ ! -f cookies.txt ]]; then
+  echo "cookies.txt is missing"
+  exit 1
+fi
+
 day=$1
 
 mkdir "$day"
-echo "#!/usr/bin/env gawk -f" > "$day/solution.awk"
-chmod +x "$day/solution.awk"
-touch "$day/input.txt"
-touch "$day/example.txt"
+echo "#!/usr/bin/env gawk -f" > "${day}/solution.awk"
+chmod +x "${day}/solution.awk"
+curl --cookie cookies.txt "https://adventofcode.com/2021/day/${day}/input" "${day}/input.txt"
+touch "${day}/example.txt"
