04748ca4b8d4b3ef8647915ea5aad01aad85e377
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-12-07 09:07:38 +0000

    fix new day script

--- 2021/new_day.sh
+++ 2021/new_day.sh
@@ -17,5 +17,5 @@ day=$1
 mkdir "$day"
 echo "#!/usr/bin/env gawk -f" > "${day}/solution.awk"
 chmod +x "${day}/solution.awk"
-curl --cookie cookies.txt "https://adventofcode.com/2021/day/${day}/input" "${day}/input.txt"
+curl -s --cookie cookies.txt "https://adventofcode.com/2021/day/${day}/input" > "${day}/input.txt"
 touch "${day}/example.txt"
