5095be37ded6f81a3a13c4272500b0e9a3197f1e
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2021-12-09 21:40:40 +0000

    create ruby solution file skeleton as well

--- 2021/new_day.sh
+++ 2021/new_day.sh
@@ -17,5 +17,13 @@ day=$1
 mkdir "$day"
 echo "#!/usr/bin/env gawk -f" > "${day}/solution.awk"
 chmod +x "${day}/solution.awk"
+
+cat << EOF > "${day}/solution.rb"
+#!/usr/bin/env ruby
+
+grid = ARGF.read
+EOF
+chmod +x "${day}/solution.rb"
+
 curl -s --cookie cookies.txt "https://adventofcode.com/2021/day/${day}/input" > "${day}/input.txt"
 touch "${day}/example.txt"
