cbc1727616dd49fe57ec00c24a83baac703e6ba8
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2024-12-02 05:44:05 +0000

    2024 1 & 2

--- 2024/1/solution.py
+++ 2024/1/solution.py
@@ -10,6 +10,21 @@ inputfile = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 D = open(inputfile).read().strip()
 L = D.split("\n")
 
-G = grid.FixedGrid.parse(D)
-R = G.height
-C = G.width
+left = []
+right = []
+
+for l in L:
+    n = nums(l)
+    left.append(n[0])
+    right.append(n[1])
+
+left.sort()
+right.sort()
+
+print(sum(abs(a - b) for a, b in zip(left, right)))
+
+p2 = 0
+for x in left:
+    p2 += right.count(x) * x
+
+print(p2)
--- 2024/2/solution.py
+++ 2024/2/solution.py
@@ -10,6 +10,29 @@ inputfile = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 D = open(inputfile).read().strip()
 L = D.split("\n")
 
-G = grid.FixedGrid.parse(D)
-R = G.height
-C = G.width
+def is_safe(n):
+    ok = n == sorted(n) or n == sorted(n, reverse=True)
+    for a, b in zip(n, n[1:]):
+        diff = abs(b - a)
+        if not 1 <= diff <= 3:
+            ok = False
+    return ok
+
+
+p1 = 0
+p2 = 0
+
+for l in L:
+    n = nums(l)
+    if is_safe(n):
+        p1 += 1
+
+    safe = False
+    for i in range(len(n)):
+        if is_safe(rm_element(n, i)):
+            safe = True
+    if safe:
+        p2 += 1
+
+print(p1)
+print(p2)
