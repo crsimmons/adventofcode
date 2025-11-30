e562f314734f89088896a05aca1c33a45d43c3da
Colin Simmons <5785492+crsimmons@users.noreply.github.com>
2025-11-30 23:02:49 +0000

    template update

--- template.py
+++ template.py
@@ -2,9 +2,12 @@
 # ruff: noqa: F405, F403, E402
 import sys
 from collections import defaultdict, deque
+from functools import lru_cache
 
 import lib.grid as grid
 from lib.util import *
+
+sys.setrecursionlimit(10**6)
 
 inputfile = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
 D = open(inputfile).read().strip()
